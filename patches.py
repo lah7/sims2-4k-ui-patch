lah7/sims2-4k-ui-patch

commit 53c72926163d17d199b4ee318120fa8768edc61b

Add option to set scale (for 2K/1440p support)
Closes #37.

patches.py
@@ -31,7 +31,7 @@ import dbpf
 from gamefile import GameFile
 
 # Density to multiply the UI dialog geometry and graphics
-UI_MULTIPLIER: int = 2
+UI_MULTIPLIER: float = 2.0
 
 # Compression keeps the package files small, but takes longer to process
 COMPRESS_PACKAGE: bool = False
@@ -85,7 +85,7 @@ def _upscale_graphic(entry: dbpf.Entry) -> bytes:
         return entry.data
 
     original = Image.open(io.BytesIO(entry.data), formats=[file_type])
-    resized = original.resize((original.width * UI_MULTIPLIER, original.height * UI_MULTIPLIER), resample=UPSCALE_FILTER)
+    resized = original.resize((int(original.width * UI_MULTIPLIER), int(original.height * UI_MULTIPLIER)), resample=UPSCALE_FILTER)
 
     output = io.BytesIO()
     resized.save(output, format=file_type)

gamefile.py
@@ -45,7 +45,9 @@ class GameFile():
         self.patched = False
         self.patch_outdated = False
         self.patched_version = 0.0
-        self.compressed = True
+
+        self.compressed: bool = True
+        self.scale: float = 2.0
 
         self.read_meta_file()
 
@@ -74,7 +76,9 @@ class GameFile():
             self.patched = True
             self.patched_version = float(config.get("patch", "version"))
             self.patch_outdated = self.patched_version < FILE_PATCH_VERSION
+
             self.compressed = config.getboolean("patch", "compressed")
+            self.scale = config.getfloat("patch", "scale")
 
     def write_meta_file(self):
         """
@@ -86,9 +90,11 @@ class GameFile():
         config = configparser.ConfigParser()
         config["patch"] = {
             "version": str(FILE_PATCH_VERSION),
-            "compressed": str(self.compressed).lower(),
+            "compressed": str(self.compressed),
+            "scale": str(self.scale),
         }
-        config.write(open(self.meta_path, "w", encoding="utf-8"))
+        with open(self.meta_path, "w", encoding="utf-8") as f:
+            config.write(f)
 
         # Prepend comment at the start of file
         with open(self.meta_path, "r+", encoding="utf-8") as f:

sims2_4k_ui_patcher.py
@@ -62,6 +62,12 @@ DEFAULT_PATHS = [
 
 PROJECT_URL = "https://github.com/lah7/sims2-4k-ui-patch"
 
+# Options
+LABELS_UI_SCALE = {
+    "200% (4K / 2160p)": 2.0,
+    "150% (2K / 1440p)": 1.5,
+}
+
 
 @staticmethod
 def get_resource(relative_path):
@@ -242,6 +248,8 @@ class PatcherApplication(tk.Tk):
         self.controls = [
             self.input_dir,
             self.btn_browse,
+            self.scale_label,
+            self.scale_option,
             self.compress_option,
             self.btn_patch,
         ]
@@ -282,6 +290,11 @@ class PatcherApplication(tk.Tk):
         self.options_label = self.widgets.make_label(self.frame_options, "Options", colour=self.widgets.colour_fg_alt)
         self.options_label.grid(row=0, column=0, padx=8, pady=2, columnspan=2, sticky=tk.W)
         self.options = []
+
+        self.scale_label = self.widgets.make_label(self.frame_options, "Scale:")
+        self.scale_option = self.widgets.make_combo(self.frame_options, list(LABELS_UI_SCALE.keys()))
+        self.scale_label.grid(row=1, column=0, padx=8, pady=4, sticky=tk.W)
+        self.scale_option.grid(row=1, column=1, padx=16, pady=4, sticky=tk.W)
 
         self.compress_option = self.widgets.make_checkbox(self.frame_options, "Compress packages", True)
         self.compress_option.grid(row=4, column=0, padx=8, pady=4, columnspan=2, sticky=tk.W)
@@ -515,6 +528,7 @@ class PatcherApplication(tk.Tk):
         Perform the patching process!
         """
         patches.COMPRESS_PACKAGE = self.widgets.is_checked(self.compress_option)
+        patches.UI_MULTIPLIER = LABELS_UI_SCALE[self.widgets.get_combo_value(self.scale_option)]
 
         self.set_status_primary("Checking permissions...", 3)
         self.set_status_secondary("")

commit 34b3f726c031f092601df4f55d10f709756660e3

Fix test regressions due to floating point
Not sure if the game supports floating point numbers, but 150% and 200%
scaling end up with whole integers, so maintain that!

Regressed from 53c72926163d17d199b4ee318120fa8768edc61b

patches.py
@@ -128,7 +128,7 @@ def _upscale_uiscript(entry: dbpf.Entry):
             new_values = []
             values = part.split("(")[1].split(")")[0]
             for number in values.split(","):
-                new_values.append(str(int(number) * UI_MULTIPLIER))
+                new_values.append(str(int(int(number) * UI_MULTIPLIER)))
             part = f"{name}={part.replace(values, ','.join(new_values))}"
             output.append(part)
         return "".join(output)
@@ -204,7 +204,7 @@ def upscale_fontstyle_ini(file: GameFile, write_meta_file=True):
             continue
 
         old_size = int(parts[3])
-        new_size = old_size * UI_MULTIPLIER
+        new_size = int(old_size * UI_MULTIPLIER)
         parts[3] = str(new_size)
         output.append('"'.join(parts))
 