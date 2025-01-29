lah7/sims2-4k-ui-patch

commit 43b55a011def3f56865d82dd49690786f4d51d2d

Fix graphics not loading outside of working directory

sims2_4k_ui_patcher.py
@@ -941,6 +941,7 @@ class QueueWindow(QDialog):
 if __name__ == "__main__":
     multiprocessing.freeze_support()  # Required for Windows
 
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
     app = QApplication(sys.argv)
     window = PatcherApplication()
 

commit 6d9bff3b7b09c37e563e0184cfb5a4a7a157d838

Fix NotADirectory exception for frozen executables
Regressed by 43b55a011def.

We don't need to set the working directory, we already have
get_resource(), it just didn't handle the path properly.

sims2_4k_ui_patcher.py
@@ -102,7 +102,7 @@ def get_resource(relative_path: str) -> str:
     """
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, relative_path) # type: ignore # pylint: disable=protected-access
-    return os.path.join(os.path.abspath("."), relative_path)
+    return os.path.join(os.path.abspath(os.path.dirname(os.path.realpath(__file__))), relative_path)
 
 
 class State:
@@ -928,8 +928,6 @@ class QueueWindow(QDialog):
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()  # Required for Windows
-
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     if os.path.exists("version.txt") and os.path.exists("lib") and os.path.exists("share"):
         # Read version for dist builds