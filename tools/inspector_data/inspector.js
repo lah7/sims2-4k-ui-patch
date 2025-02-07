6cf2fc8ec6d14884f6c04f226ae9207d8a3bdad0
Luke Horwell <code@horwell.me>
2025-02-07 18:59:50 +0000

    Tools: Initial inspector to analyse .uiscript files
    Since .uiscript files are modified versions of XML, HTML is the closest
    language to easily work with them. With a bit of CSS/JS, they can be
    styled in a web view for inspection.
    
    A tool sat on the backburner for a long time, originally using Qt
    widgets, but now using QWebEngine.
    
    The tool can be refined over time, but it'll give a helping hand in
    figuring out how the game puts UI together to come up with solutions to
    our outstanding issues.

---
+++ tools/inspector_data/inspector.js
@@ -0,0 +1,100 @@
+// Bridge with Python for retrieving images
+let python;
+new QWebChannel(qt.webChannelTransport, function(channel) {
+    python = channel.objects.python;
+});
+
+window.onload = function() {
+    // Wait for QWebChannel to be ready
+    if (typeof python === "undefined") {
+        setTimeout(window.onload, 100);
+        return;
+    }
+
+    // Render each element roughly based on the UI script.
+    // Many attributes origin from older Maxis games and may not be used.
+    document.querySelectorAll(".LEGACY").forEach((element) => {
+        const clsid = element.getAttribute("clsid");
+        const iid = element.getAttribute("iid");
+
+        // Parse UI script attributes
+        const _area = element.getAttribute("area") ? element.getAttribute("area").slice(1, -1).split(',') : [0,0,0,0]; // (startX, startY, endX, endY)
+        const area = {
+            x: parseInt(_area[0]),
+            y: parseInt(_area[1]),
+            width: parseInt(_area[2]) - parseInt(_area[0]),
+            height: parseInt(_area[3]) - parseInt(_area[1]),
+        };
+
+        const _gutters = element.getAttribute("gutters") ? element.getAttribute("gutters") : [0,0,0,0]; // (left, top, right, bottom) or (left/right, top/bottom)
+        const gutters = {
+            left: parseInt(_gutters[0]),
+            top: parseInt(_gutters[1]),
+            right: parseInt(_gutters[2]),
+            bottom: parseInt(_gutters[3]),
+        };
+
+        const _fillcolor = `rgb${element.getAttribute("fillcolor")}`;
+        const _bkgcolor = `rgb${element.getAttribute("bkgcolor")}`;
+        const background = element.getAttribute("fillcolor") ? _fillcolor : _bkgcolor;
+        const forecolor = `rgb${element.getAttribute("forecolor")}`;
+
+        const image = element.getAttribute("image");
+        const caption = element.getAttribute("caption");
+        const noShowCaption = element.getAttribute("showcaption") == "no";
+        const tips = element.getAttribute("tips") === "yes" || false;
+        const align = element.getAttribute("align") || "left"; // left, right, center, lefttop
+
+        // Transform into HTML layout
+        element.classList.add(element.getAttribute("clsid"));
+
+        /* Area */
+        element.style.position = "absolute";
+        element.style.top = `${area.y}px`;
+        element.style.left = `${area.x}px`;
+        element.style.height = `${area.height}px`;
+        element.style.width = `${area.width}px`;
+
+        /* Gutters */
+        element.style.paddingTop = `${gutters.top}px`;
+        element.style.paddingRight = `${gutters.right}px`;
+        element.style.paddingBottom = `${gutters.bottom}px`;
+        element.style.paddingLeft = `${gutters.left}px`;
+
+        /* Colours */
+        element.style.color = forecolor;
+        if (iid === "IGZWinCustom")
+            element.style.backgroundColor = background;
+
+        /* Load images */
+        if (image) {
+            python.get_image(image, function(b64data) {
+                if (!b64data && element.children.length === 0) {
+                    element.style.backgroundColor = "red";
+                    element.classList.add("missing");
+                    return;
+                }
+                const style = document.createElement("style");
+                style.innerHTML = `.${clsid}[image="${image}"] { background-image: url(data:image/png;base64,${b64data}); }`;
+                document.body.appendChild(style);
+            });
+        }
+
+        /* Show text unless it seems likely to be a image button or contains technical key/value data */
+        if (caption && !noShowCaption && !tips && caption.search("=") === -1) {
+            element.innerText = element.getAttribute("caption");
+        }
+
+        if (tips)
+            element.setAttribute("title", element.getAttribute("tiptext"));
+    });
+
+    // Children should be nested inside the previous LEGACY element
+    document.querySelectorAll(".CHILDREN").forEach((children) => {
+        const parent = children.previousElementSibling;
+        children.querySelectorAll(".LEGACY").forEach((child) => {
+            parent.appendChild(child);
+        });
+        children.remove();
+    });
+};
---
+++ tools/uiscript_inspector_qt.py
@@ -0,0 +1,274 @@
+#!/usr/bin/python3
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2022, 2024-2025 Luke Horwell <code@horwell.me>
+#
+"""
+Qt interface for browsing and recreating user interfaces from
+The Sims 2 by parsing .uiScript files and associated graphics.
+
+Requires QWebEngineView.
+"""
+import base64
+import io
+import os
+import re
+import signal
+import sys
+
+import PIL.Image
+
+# Our modules are in the parent directory
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) # pylint: disable=wrong-import-position
+
+from PyQt6.QtCore import QObject, Qt, QTimer, QUrl, pyqtSlot
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWebChannel import QWebChannel
+from PyQt6.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtWidgets import (QAbstractScrollArea, QApplication, QDockWidget,
+                             QFileDialog, QHBoxLayout, QMainWindow, QSplitter,
+                             QStatusBar, QTreeWidget, QTreeWidgetItem, QWidget)
+
+from sims2patcher import dbpf
+
+DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "inspector_data"))
+
+
+def uiscript_to_html(orig: str) -> str:
+    """
+    Convert .uiscript files to plain HTML.
+    UI Scripts are XML-like formats with unquoted attribute values.
+
+    Example:
+        <LEGACY clsid=GZWinGen iid=IGZWinGen area=(10,10,605,432) >
+
+    Becomes:
+        <div class="GZWinGen" id="IGZWinGen" x="10" y="10" width="605" height="432"></div>
+    """
+    output = ""
+    for line in orig.split("\n"):
+        if line.startswith("#"):
+            continue
+        output += line + "\n"
+
+    # Replace <LEGACY> and <CHILDREN> tags with <div>
+    output = re.sub(r'<(\w+)', r'<div class="\1"', output)
+
+    # <LEGACY> tags didn't have closing tags, add one if not present
+    output = output.replace(" >", "></div>")
+
+    return output
+
+
+class State:
+    """A collection of entries from a .package file"""
+    def __init__(self):
+        self.package = dbpf.DBPF()
+        self.package_path = ""
+        self.ui_files: list[dbpf.Entry] = []
+        self.graphics: list[dbpf.Entry] = []
+
+
+class Bridge(QObject):
+    """Bridge between Python and JavaScript"""
+    def __init__(self, state: State):
+        super().__init__()
+        self.state = state
+
+    @pyqtSlot(str, result=str) # type: ignore
+    def get_image(self, image_attr: str) -> str:
+        """Return a base64 encoded string of a graphic file from the package. Read the image attribute directly."""
+        try:
+            _group_id, _instance_id = image_attr[1:-1].split(",")
+            group_id = int(_group_id, 16)
+            instance_id = int(_instance_id, 16)
+        except ValueError:
+            print(f"Invalid image ID: {image_attr}")
+            return ""
+
+        try:
+            entry = self.state.package.get_entry(dbpf.TYPE_IMAGE, group_id, instance_id)
+        except ValueError:
+            print(f"Image not found: Group ID {group_id}, Instance ID {instance_id}")
+            return ""
+
+        # Convert to PNG as browser doesn't support TGA
+        try:
+            io_in = io.BytesIO(entry.data_safe)
+            io_out = io.BytesIO()
+            tga = PIL.Image.open(io_in)
+            tga = tga.convert("RGBA") # Remove transparency
+            tga.save(io_out, format="PNG")
+            return base64.b64encode(io_out.getvalue()).decode("utf-8")
+        except dbpf.errors.QFSError:
+            print(f"Image failed to extract: Group ID {group_id}, Instance ID {instance_id}")
+            return ""
+
+
+class MainInspectorWindow(QMainWindow):
+    """Main interface for inspecting .uiScript files"""
+    def __init__(self):
+        super().__init__()
+
+        # Variables
+        self.state = State()
+        self.items: list[QTreeWidgetItem] = []
+
+        # Layout
+        self.base_widget = QWidget()
+        self.base_layout = QHBoxLayout()
+        self.base_widget.setLayout(self.base_layout)
+        self.setCentralWidget(self.base_widget)
+
+        self.file_tree = QTreeWidget(self.base_widget)
+        self.file_tree.setSizeAdjustPolicy(QAbstractScrollArea.SizeAdjustPolicy.AdjustToContentsOnFirstShow)
+        self.file_tree.setHeaderLabels(["Group ID", "Instance ID", "Caption Hint"])
+        self.file_tree.setColumnWidth(0, 100)
+        self.file_tree.setColumnWidth(1, 100)
+        self.file_tree.setColumnWidth(2, 150)
+        self.file_tree.setSortingEnabled(True)
+        self.file_tree.currentItemChanged.connect(self.open_ui_file)
+
+        self.list_dock = QDockWidget("UI Files", self)
+        self.list_dock.setMinimumWidth(400)
+        self.list_dock.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetFloatable | QDockWidget.DockWidgetFeature.DockWidgetMovable)
+        self.list_dock.setWidget(self.file_tree)
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.list_dock)
+
+        self.status_bar: QStatusBar = self.statusBar() # type: ignore
+        self.status_bar.showMessage("Loading...")
+
+        # UI Preview
+        self._webview = QWidget()
+        self._webview_layout = QHBoxLayout()
+        self._webview_layout.setContentsMargins(0, 0, 0, 0)
+        self._webview.setLayout(self._webview_layout)
+        self.webview = QWebEngineView(self._webview)
+        self._webview_layout.addWidget(self.webview)
+        self.webview.setContextMenuPolicy(Qt.ContextMenuPolicy.NoContextMenu)
+        self.webview.setHtml("<style>body { background: #003062; }</style>")
+
+        # Inspector View
+        self._inspector = QWidget()
+        self._inspector_layout = QHBoxLayout()
+        self._inspector_layout.setContentsMargins(0, 0, 0, 0)
+        self._inspector.setLayout(self._inspector_layout)
+        self.inspector = QWebEngineView(self._inspector)
+        self._inspector_layout.addWidget(self.inspector)
+        self.inspector.setContextMenuPolicy(Qt.ContextMenuPolicy.DefaultContextMenu)
+        self.inspector.page().setInspectedPage(self.webview.page()) # type: ignore
+
+        self.web_splitter = QSplitter(Qt.Orientation.Horizontal)
+        self.web_splitter.addWidget(self._webview)
+        self.web_splitter.addWidget(self._inspector)
+        self.web_splitter.setSizes([1000, 500])
+        self.base_layout.addWidget(self.web_splitter)
+
+        # The bridge allows the web view to communicate with Python
+        self.channel = QWebChannel()
+        self.webview.page().setWebChannel(self.channel) # type: ignore
+
+        # Register bridge object
+        self.bridge = Bridge(self.state)
+        self.channel.registerObject("python", self.bridge)
+
+        # Window properties
+        self.resize(1424, 768)
+        self.setWindowTitle("UI Inspector for The Sims 2 by lah7")
+        self.setWindowIcon(QIcon(os.path.abspath(os.path.join(DATA_DIR, "..", "..", "assets", "status_default@2x.png"))))
+        self.show()
+
+        # Auto load package when passed as a command line argument
+        if len(sys.argv) > 1:
+            self.state.package_path = sys.argv[1]
+            if os.path.exists(self.state.package_path):
+                self.load_files()
+        else:
+            self.browse()
+
+    def browse(self):
+        """Show the file dialog to select a package file"""
+        browser = QFileDialog(self)
+        browser.setFileMode(QFileDialog.FileMode.ExistingFile)
+        browser.setNameFilter("The Sims 2 Package Files (*.package CaSIEUI.data)")
+        browser.setViewMode(QFileDialog.ViewMode.Detail)
+
+        if browser.exec() == QFileDialog.DialogCode.Accepted:
+            self.state.package_path = browser.selectedFiles()[0]
+            self.load_files()
+        else:
+            sys.exit(130)
+
+    def load_files(self):
+        """Load the .uiScript files from the selected package"""
+        self.status_bar.showMessage(f"Reading: {self.state.package_path}")
+        self.setCursor(Qt.CursorShape.WaitCursor)
+        QApplication.processEvents()
+
+        self.state.package = dbpf.DBPF(self.state.package_path)
+        self.state.ui_files = [entry for entry in self.state.package.entries if entry.type_id == dbpf.TYPE_UI_DATA]
+        self.state.graphics = [entry for entry in self.state.package.entries if entry.type_id == dbpf.TYPE_IMAGE]
+        total = len(self.state.ui_files)
+
+        for entry in self.state.ui_files:
+            item = QTreeWidgetItem(self.file_tree, [str(hex(entry.group_id)), str(hex(entry.instance_id)), ""])
+            item.setData(0, Qt.ItemDataRole.UserRole, entry)
+            self.items.append(item)
+
+        self.status_bar.showMessage(f"Listed {total} UI files from {self.state.package_path}", 3000)
+        self.setCursor(Qt.CursorShape.ArrowCursor)
+
+        timer = QTimer(self)
+        timer.singleShot(1, self.preload_files)
+
+    def open_ui_file(self, item: QTreeWidgetItem):
+        """Open the selected .uiScript file in the web view"""
+        entry: dbpf.Entry = item.data(0, Qt.ItemDataRole.UserRole)
+        try:
+            html = uiscript_to_html(entry.data.decode("utf-8"))
+        except UnicodeDecodeError:
+            html = "Unable to decode. It may be binary data."
+        with open(os.path.join(DATA_DIR, "inspector.html"), "r", encoding="utf-8") as f:
+            html = f.read().replace("PLACEHOLDER", html)
+        self.webview.setHtml(html, baseUrl=QUrl.fromLocalFile(f"{DATA_DIR}/"))
+
+    def preload_files(self):
+        """Continue loading files in the background to identify captions and binary files"""
+        for item in self.items:
+            entry: dbpf.Entry = item.data(0, Qt.ItemDataRole.UserRole)
+            if entry.decompressed_size > 1024 * 1024: # Likely binary (over 1 MiB)
+                item.setDisabled(True)
+                item.setText(2, "Binary data")
+                continue
+
+            html = entry.data.decode("utf-8")
+            matches = re.findall(r'\bcaption="([^"]+)"', html)
+
+            # Use longest caption as the title
+            if matches:
+                # Exclude captions used for technical key/value pairs
+                matches = [match for match in matches if not match.find("=") != -1 and not match.isupper()]
+
+                item.setText(2, max(matches, key=len))
+                item.setToolTip(2, "\n".join(matches))
+
+
+if __name__ == "__main__":
+    # CTRL+C to exit
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    app = QApplication(sys.argv)
+    window = MainInspectorWindow()
+    app.exec()
